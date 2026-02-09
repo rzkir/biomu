c3a1a7b320797dc1f7c60e992bd23903164ea66e
rzkir <rr8027896@gmail.com>
2026-02-10 06:21:16 +0700

    Initial commit from Create Next App

---
+++ helper/routing/Pathname.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { Fragment } from "react";
+
+import { usePathname } from "next/navigation";
+
+import Header from "@/helper/layout/Header";
+
+import { Toaster } from "sonner";
+
+const Pathname = ({ children }: { children: React.ReactNode }) => {
+    const pathname = usePathname();
+
+    const isRoute =
+        pathname?.includes("/signin") ||
+        pathname?.includes("/signup") ||
+        pathname?.includes("/forgot-password") ||
+        pathname?.includes("/verification") ||
+        pathname?.includes("/change-password") ||
+        pathname?.includes("/reset-password") ||
+        pathname?.includes("/dashboard") ||
+        false;
+
+
+    return (
+        <Fragment>
+            <Toaster
+                position="top-center"
+                richColors
+                toastOptions={{
+                    duration: 3000,
+                    className: "font-medium",
+                }}
+            />
+            {!isRoute && <Header />}
+            {children}
+        </Fragment>
+    );
+};
+
+export default Pathname;
